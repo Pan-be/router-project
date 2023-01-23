62ad1c59315571f738340a1529ae3a3384240d09
Konrad <konradbieniek@gmail.com>
2023-01-23 23:47:24 +0100

    reused the same action to add and edit event

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import EventsPage, { loader as eventsLoader } from "./pages/EventsPage"
-import NewEventPage, { action as newEventAction } from "./pages/NewEventPage"
+import NewEventPage from "./pages/NewEventPage"
 import EventDetailPage, {
 	loader as eventLoader,
 	action as deleteEventAction,
@@ -31,6 +31,7 @@ import EditEventPage from "./pages/EditEventPage"
 import RootLayout from "./pages/Root"
 import EventsRoot from "./pages/EventsRoot"
 import ErrorPage from "./pages/Error"
+import { action as manipulateEventAction } from "./components/EventForm"
 
 const router = createBrowserRouter([
 	{
@@ -58,10 +59,18 @@ const router = createBrowserRouter([
 								element: <EventDetailPage />,
 								action: deleteEventAction,
 							},
-							{ path: "edit", element: <EditEventPage /> },
+							{
+								path: "edit",
+								element: <EditEventPage />,
+								action: manipulateEventAction,
+							},
 						],
 					},
-					{ path: "new", element: <NewEventPage />, action: newEventAction },
+					{
+						path: "new",
+						element: <NewEventPage />,
+						action: manipulateEventAction,
+					},
 				],
 			},
 		],
