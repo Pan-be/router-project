fdb96039d39ceceb4c86aa616931628f19c7da7d
Konrad <konradbieniek@gmail.com>
2023-02-09 11:48:52 +0100

    token handling

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,19 +1,25 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { useEffect } from "react"
+import { Outlet, useLoaderData, useNavigation } from "react-router-dom"
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation"
 
 function RootLayout() {
-  // const navigation = useNavigation();
-
-  return (
-    <>
-      <MainNavigation />
-      <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-        <Outlet />
-      </main>
-    </>
-  );
+	const token = useLoaderData()
+	// const navigation = useNavigation();
+	useEffect(() => {
+		if (!token) {
+			return
+		}
+	}, [token])
+	return (
+		<>
+			<MainNavigation />
+			<main>
+				{/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+				<Outlet />
+			</main>
+		</>
+	)
 }
 
-export default RootLayout;
+export default RootLayout
